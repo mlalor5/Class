12f8cef5c81c8e9a8eeb61128879f6fa83b8f1d4
eozdemir <eozdemir@wustl.edu>
2014-01-30 14:33:26 -0600

    Start
    over

--- ClassExample.R
+++ ClassExample.R
@@ -1,16 +1,3 @@
 
 
-print("Hello World")
-
-
-print("A new line")
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-
-print("Third Line")
-=======
-print("I took over this file")
->>>>>>> b38cf21c1f0cdff53eb1cb8276622b778f5fc1fa
->>>>>>> c3d66aa06fe9c81918ab0e2f88f0eb3d127900fd
+print("Start over")
